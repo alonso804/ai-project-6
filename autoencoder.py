6e8a4ecd958ef9812cae3c4fb5db941f1de30dd4
alonso804 <alonsobs804@gmail.com>
2021-12-13 23:58:35 -0500

    update

---
+++ autoencoder.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transform
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.f1 = nn.Conv2d(4, 16, 3, stride=2, padding=1)  # 128
+        self.f2 = nn.Conv2d(16, 32, 3, stride=2, padding=1)  # 64
+        self.f3 = nn.Conv2d(32, 64, 32)
+
+    def forward(self, image):
+        #print('out0:', image.shape)
+        out = F.relu(self.f1(image))
+        #print('out1:', out.shape)
+        out = F.relu(self.f2(out))
+        #print('out2:', out.shape)
+        out = F.relu(self.f3(out))
+        #print('out3:', out.shape)
+        z = F.relu(out)
+        return z
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.f1 = nn.ConvTranspose2d(64, 32, 32)
+        self.f2 = nn.ConvTranspose2d(
+            32, 16, 3, stride=2, padding=1, output_padding=1)
+        self.f3 = nn.ConvTranspose2d(
+            16, 4, 3, stride=2, padding=1, output_padding=1)
+
+    def forward(self, z):
+        out = F.relu(self.f1(z))
+        out = F.relu(self.f2(out))
+        out = F.relu(self.f3(out))
+        out = torch.tanh(out)
+        return out
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, image):
+        z = self.encoder(image)
+        out = self.decoder(z)
+        return out
